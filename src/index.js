547de9abb02638db4bb0da70e6485c30497af8f5
Stephen Cook <stephen@stephencookdev.co.uk>
2019-06-29 12:13:22 +0100

    Make the code samples be in light mode, with a dev toggle to dark mode

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("mount"));
+
+console.log("Some dev shortcuts:");
+console.table({
+  "toggleDarkMode()": "Change code samples between light/dark mode"
+});
--- src/misc/CustomSlide/index.js
+++ src/misc/CustomSlide/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { ContentSlide } from "react-presents";
 import classNames from "classnames";
 import styles from "./styles.scss";
 
-export default ({ children, title }) => (
-  <ContentSlide
-    className={classNames(styles["custom-slide"], {
-      [styles["title-slide"]]: title
-    })}
-  >
-    {children}
-  </ContentSlide>
-);
+export default ({ children, title }) => {
+  const [darkMode, setDarkMode] = useState(!!localStorage.getItem("darkMode"));
+  window.toggleDarkMode = () => {
+    if (darkMode) {
+      localStorage.removeItem("darkMode");
+    } else {
+      localStorage.setItem("darkMode", 1);
+    }
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ContentSlide
+      className={classNames(styles["custom-slide"], {
+        [styles["title-slide"]]: title,
+        [styles["light-mode"]]: !darkMode
+      })}
+    >
+      {children}
+    </ContentSlide>
+  );
+};
