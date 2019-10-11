8a8f24e496344852a26c99fd7a96ef9f7f2773be
Stephen Cook <stephen@stephencookdev.co.uk>
2019-10-11 10:21:01 +0100

    Prerender the index.html page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("mount"));
+document.dispatchEvent(new Event("post-react-render"));
 
 console.log("Some dev shortcuts:");
 console.table({
